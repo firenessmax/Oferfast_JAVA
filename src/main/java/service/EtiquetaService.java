010965d4e5c398ea663d000e21c39c529a859af4
Cristian Espinoza Leon <cristian.espinozal@usach.cl>
2015-12-28 05:50:10 -0300

    /etiquetas/{id}/ofertas
    todas las ofertas de alguna etiqueta

--- src/main/java/service/EtiquetaService.java
+++ src/main/java/service/EtiquetaService.java
@@ -1,5 +1,6 @@
 package service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -13,7 +14,10 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import facade.EtiquetaFacade;
+import facade.OfertaHasEtiquetaFacade;
+import facade.OfertaFacade;
 import model.Etiqueta;
+import model.OfertaHasEtiqueta;
 import model.Oferta;
 
 @Path("/etiquetas")
@@ -21,6 +25,12 @@ public class EtiquetaService {
 
 	@EJB 
 	EtiquetaFacade etiquetaFacadeEJB;
+
+	@EJB 
+	OfertaHasEtiquetaFacade ofertaHasEtiquetaFacadeEJB;
+
+	@EJB 
+	OfertaFacade ofertaFacadeEJB;
 	
 	Logger logger = Logger.getLogger(EtiquetaService.class.getName());
 	
@@ -40,8 +50,21 @@ public class EtiquetaService {
     @GET
     @Path("{id}/ofertas")
     @Produces({"application/xml", "application/json"})
-    public List<Oferta> find_ofertas(@PathParam("id") Integer id) {
-        return etiquetaFacadeEJB.find(id).getListaSoloOfertas();
+    public List<Oferta> findOfertas(@PathParam("id") Integer id) {
+    	List<OfertaHasEtiqueta> listaOfertaHasEtiqueta = ofertaHasEtiquetaFacadeEJB.findAll(); //relacion oferta-etiqueta
+    	List<Oferta> listaOferta = ofertaFacadeEJB.findAll();//se obtienen todas las ofertas
+    	List<Oferta> respuesta = new ArrayList<>(); //lista para la respuesta
+    	for(int i=0; i<listaOfertaHasEtiqueta.size(); i++){
+        	if(listaOfertaHasEtiqueta.get(i).getEtiquetaId() == id ){ //la oferta-etiqueta tiene la misma etiquetaId que la id actual
+        		for(int j=0; j<listaOfertaHasEtiqueta.size(); j++){
+            		if(listaOferta.get(j).getOfertaId() == listaOfertaHasEtiqueta.get(i).getOfertaId()){
+            			respuesta.add(listaOferta.get(j));
+            			break;
+            		}
+            	}
+        	}
+        }
+       return respuesta;
     }
 	
 	@POST
